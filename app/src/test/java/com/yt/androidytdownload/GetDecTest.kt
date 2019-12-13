fe5d4df05fd3bce7667dd6248627c28441b765b2
Adam_Z <youngtoss38@gmail.com>
2019-12-13 12:08:43 +0100

    test added for praseffmpeg fun

--- app/src/test/java/com/yt/androidytdownload/GetDecTest.kt
+++ app/src/test/java/com/yt/androidytdownload/GetDecTest.kt
@@ -4,6 +4,7 @@ import com.google.gson.Gson
 import com.yt.androidytdownload.Model.VideoDetails
 import com.yt.androidytdownload.util.GetDecFromStr
 import com.yt.androidytdownload.util.deleteWhen
+import com.yt.androidytdownload.util.parseFFMpegOnProgressStr
 import com.yt.androidytdownload.util.strToJson
 import org.junit.Assert
 import org.junit.Test
@@ -12,7 +13,7 @@ import org.junit.Assert.*
 class GetDecTest {
 
     @Test
-    fun getDec_isCorrect(){
+    fun getDec_isCorrect() {
 
 
         assertEquals(123, GetDecFromStr("123"))
@@ -25,21 +26,40 @@ class GetDecTest {
 
 
     @Test
-    fun deleteWhenTest(){
-        val string = "{\"title\": \"Konrad \\ud83c\\udd9a Kurian \\ud83c\\udfa4 WBW 2019 Fina\\u0142 (freestyle rap battle)\", \"file_size\": 31.760437}���������������������������������������������������������������������������������������������������������������������������������������������"
+    fun deleteWhenTest() {
+        val string =
+            "{\"title\": \"Konrad \\ud83c\\udd9a Kurian \\ud83c\\udfa4 WBW 2019 Fina\\u0142 (freestyle rap battle)\", \"file_size\": 31.760437}���������������������������������������������������������������������������������������������������������������������������������������������"
 
-        assertEquals("{\"title\": \"Konrad \\ud83c\\udd9a Kurian \\ud83c\\udfa4 WBW 2019 Fina\\u0142 (freestyle rap battle)\", \"file_size\": 31.760437}",
-            deleteWhen(string,'}')
+        assertEquals(
+            "{\"title\": \"Konrad \\ud83c\\udd9a Kurian \\ud83c\\udfa4 WBW 2019 Fina\\u0142 (freestyle rap battle)\", \"file_size\": 31.760437}",
+            deleteWhen(string, '}')
         )
 
-        val title = Gson().fromJson(deleteWhen(string,'}'),VideoDetails::class.java)
+        val title = Gson().fromJson(deleteWhen(string, '}'), VideoDetails::class.java)
     }
 
 
     @Test
-    fun check(){
-        strToJson("{\"title\": \"Konrad \\ud83c\\udd9a Kurian \\ud83c\\udfa4 WBW 2019 Fina\\u0142 (freestyle rap battle)\", \"file_size\": 31.760437}",VideoDetails::class.java)
+    fun check() {
+        strToJson(
+            "{\"title\": \"Konrad \\ud83c\\udd9a Kurian \\ud83c\\udfa4 WBW 2019 Fina\\u0142 (freestyle rap battle)\", \"file_size\": 31.760437}",
+            VideoDetails::class.java
+        )
     }
+
+    @Test
+    fun ifEqualsAndNoExceptionThenOk() {
+
+        val str1 = " Progress:size=    2456kB time=00:02:37.15 bitrate= 128.0kbits/s speed=1.48x"
+        val str2 = "Progress:size=    2323kB time=00:02:28.63 bitrate= 128.0kbits/s speed=1.48x"
+        val str3 = "Progress:size=      10kB time=00:00:00.60 bitrate= 137.9kbits/s speed=1.16x"
+
+        assertEquals(2456, parseFFMpegOnProgressStr(str1))
+        assertEquals(2323, parseFFMpegOnProgressStr(str2))
+        assertEquals(10, parseFFMpegOnProgressStr(str3))
+        
+    }
+
 
 }
 
